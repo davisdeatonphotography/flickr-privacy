b8cfb9e323eb20ecdea2aceafc868e782f316210
davisdeatonphotography <130251632+davisdeatonphotography@users.noreply.github.com>
2023-07-24 04:30:15 -0500

    v 0.1.

---
+++ push.ps1
@@ -0,0 +1,7 @@
+$env:GIT_ASKPASS = "echo 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"; `
+git add .; `
+git commit -m "v 0.1."; `
+git push origin master; `
+Remove-Item Env:\GIT_ASKPASS; `
+heroku git:remote -a flickr-privacy; `
+git push heroku master
