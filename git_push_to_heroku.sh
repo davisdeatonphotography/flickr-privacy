c032893d50fabc98b2909dc52d504f4b3419d3b5
Davis <davisaitools@gmail.com>
2023-07-24 03:56:28 -0500

    v 0.1.

--- git_push_to_heroku.sh
+++ git_push_to_heroku.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Add all changed files to git
 git add .
 
@@ -7,7 +5,7 @@ git add .
 git commit -m "v 0.1."
 
 # Push the changes to your GitHub repository using the personal access token
-git push origin master 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
+git push origin master
 
 # Set up remote for Heroku (only needs to be done once)
 heroku git:remote -a flickr-privacy
