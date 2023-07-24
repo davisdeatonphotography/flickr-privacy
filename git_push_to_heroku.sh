29c1f7d6eb4f32e2afea4e73d0192696403e1ed9
Davis <davisaitools@gmail.com>
2023-07-24 04:08:44 -0500

    v 0.1.

--- git_push_to_heroku.sh
+++ git_push_to_heroku.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+# Set up the Git AskPass script to provide the personal access token
+GIT_ASKPASS_SCRIPT="$(mktemp)"
+cat << 'EOF' > "${GIT_ASKPASS_SCRIPT}"
+#!/bin/bash
+echo "$PERSONAL_ACCESS_TOKEN"
+EOF
+chmod +x "${GIT_ASKPASS_SCRIPT}"
+export GIT_ASKPASS="${GIT_ASKPASS_SCRIPT}"
+
 # Add all changed files to git
 git add .
 
 # Commit the changes with a message
 git commit -m "v 0.1."
 
-# Set up Git credentials for GitHub using your personal access token
-git config credential.helper 'store --file ~/.git-credentials'
-echo "https://github.com:davisdeatonphotography:${PERSONAL_ACCESS_TOKEN}" >> ~/.git-credentials
+# Push the changes to your GitHub repository using the personal access token
+PERSONAL_ACCESS_TOKEN="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" git push origin master
 
-# Push the changes to your GitHub repository
-git push origin master
+# Clean up the Git AskPass script
+rm -f "${GIT_ASKPASS_SCRIPT}"
 
 # Set up remote for Heroku (only needs to be done once)
 alias heroku=/usr/local/bin/heroku
